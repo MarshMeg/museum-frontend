dc6448529ed48693241b3a8498d4a4032a8e86de
Marsh Meg <uraevdmitrij031@gmail.com>
2024-02-21 16:42:26 +0400

    SEO optimisation

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,22 @@ import BasePage from "./pages/BasePage.jsx";
 import "./App.css"
 
 
+function ReAdmin() {
+  console.log(document.URL)
+  window.location.url = "https://dimasik911.pythonanywhere.com/admin/"
+  return (
+    <>
+    </>
+  )
+}
+
+
 export default function App() {
   return (
     <>
       <Routes>
         <Route index path={ "/:lang/*" } element={<BasePage />} />
+        <Route path={ "/admin" } element={<ReAdmin />} />
         <Route path={ "*" } element={<Navigate to="/ru/main" replace />} />
       </Routes>
     </>
